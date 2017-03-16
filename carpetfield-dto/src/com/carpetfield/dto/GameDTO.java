157b1b83971da359e9abe3f47c58843d810e2de4
skiranoglu <seckinkiranoglu@gmail.com>
2017-03-16 16:22:05 +0300

    dto project

---
+++ carpetfield-dto/src/com/carpetfield/dto/GameDTO.java
@@ -0,0 +1,25 @@
+package com.carpetfield.dto;
+
+/**
+ * Created by seckink on 3/16/2017.
+ */
+public class GameDTO {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
